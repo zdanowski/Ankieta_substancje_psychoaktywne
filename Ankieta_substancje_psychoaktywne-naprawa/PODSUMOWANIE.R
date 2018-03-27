9aecd5dc68b55e45f5bd23d10025ebbd223184be
zdanowski <zdanowski@users.noreply.github.com>
2018-03-27 17:42:59 +0200

    naprawa problemu z podsumowaniem

---
+++ Ankieta_substancje_psychoaktywne-naprawa/PODSUMOWANIE.R
@@ -0,0 +1,94 @@
+# PODSUMOWANIE
+# Analizy zmiennych ilościowych dokonano za pomocą testu U Manna-Whitneya oraz testu Chi Kwadrat. 
+# Analizy zmiennych binarnych dokonano za pomocą testu Chi Kwadrat.
+
+
+# 1. Porównanie testem U manna-whitneya czy jest statystycznie istotna różnica 
+# >>>>>użyto testu U manna whitney (wilcoxona), ponieważ zmienne te nie mają rozkładu normalnego (ciężko żeby miały przy 5 wartośćiach)
+# między populacją osób sięgających po substancje nielegalne w odpowiedzi na pytania:
+# namawianie = W jakim stopniu zgadzasz się ze stwierdzeniem: Ktoś usilnie namawiał mnie do pójścia na studia/wybrania tego kierunku.
+# ciekawe = W jakim stopniu zgadzasz się ze stwierdzeniem: Studia na które uczęszczam uważam za ciekawe i interesujące.
+load(file = "dane.Rda")
+legal <-
+  subset.data.frame(dane, subset = dane$nielegal_ogol == 0) #subset NIEsiegajacych po nielegalne substancje
+nielegal <-
+  subset.data.frame(dane, subset = dane$nielegal_ogol == 1)#subset siegajacych po nielegalne substancje
+
+#######W jakim stopniu zgadzasz się ze stwierdzeniem: Ktoś usilnie namawiał mnie do pójścia na studia/wybrania tego kierunku.
+wilcox.test(legal$Namawianie, nielegal$Namawianie)
+#Hipoteza zerowa - nie ma istonej różnicy między grupami.
+#Wilcoxon rank sum test with continuity correction
+#data:  legal$Namawianie and nielegal$Namawianie
+#W = 17010, p-value = 0.01582 - HIPOTEZA ZEROWA ODRZUCONA
+#alternative hypothesis: true location shift is not equal to 0
+
+
+######W jakim stopniu zgadzasz się ze stwierdzeniem: Moje studia uważam za ciekawe i interesujące.
+wilcox.test(legal$Ciekawe, nielegal$Ciekawe)
+#Hipoteza zerowa - nie ma istonej różnicy między grupami.
+#data:  legal$Ciekawe and nielegal$Ciekawe
+#W = 23814, p-value = 0.03942 HIPOTEZA ZEROWA ODRZUCONA
+#alternative hypothesis: true location shift is not equal to 0
+
+#Porównanie wieku miedzy grupami
+wilcox.test(legal$Wiek, nielegal$Wiek) 
+
+#Porównanie roku studiów między grupami
+wilcox.test(legal$rok, nielegal$rok) 
+
+
+
+
+print("__________________________________________________________________________________________________", quote = FALSE)
+
+#2.
+#Tu zaczna się skrypt z testem ChiKwadrat.
+#Analizuję tu zależność między zmiennymi różnymi zmiennymi binarnymi (w przypadku "Namawianie" i "ciekawe" które są w skali likerta) a zmienną "nielegal_ogol".
+#Pytanie: czemu w przypadku niektórych zmiennych wyskakuje komunikat "In chisq.test(tab) : Chi-squared approximation may be incorrect"?
+#Te same dane wporwadzone do statistici dają takie same wyniki, ale bez komunikatu o błędzie.
+#WSZYSTKIE KOLUMNY Dane.Rda:
+#[1] "lp"                          "Wiek"                        "M=0/K=1"                     "Student"                     "Kierunek"
+#[6] "rok"                         "Namawianie"                  "Ciekawe"                     "Substancje"                  "Medycyna"
+#[11] "Kierunek_medyczny"           "Kofeina"                     "Nikotyna"                    "alkohol"                     "napoje_energertyzujące"
+#[16] "dopalacze"                   "amfetamina"                  "kokaina"                     "marihuana/haszysz"           "srod_uspok_niezap_przez_lek"
+#[21] "inne"                        "inne_nielegal"               "Podstawowe_nielegal"         "nielegal_ogol"
+
+istotnenumer <- c()
+istotnenazwa <- c()
+
+v <- c(6, 7, 8, 10, 11, 12, 13, 14, 15, 16)
+for (i in v) {
+  n <- as.vector(t(dane[i]))
+  print(paste(i, "-", names(dane[i]), " a nielegal_ogol"))
+  
+  tab <- table(n, dane$nielegal_ogol)
+  chi <- chisq.test(tab)
+  print(chi)
+  p <- chi$p.value
+  if (p < 0.05) {
+    print(
+      " p < 0.5 ISTOTNOŚC!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
+      quote = FALSE
+    )
+    istotnenumer <-
+      c(istotnenumer, i) #Tworzy wektor z numerami kolumn(zmiennych) dla których p < 0.5
+    istotnenazwa <-
+      c(istotnenazwa, names(dane[i])) #Tworzy wektor z nazwami kolumn(zmiennych) dla których p < 0.5
+  }
+  print(
+    "_____________________________________________________________________________________________________________________________________________________-"
+    ,
+    quote = FALSE
+  ) #może da się to inaczej zrobić?
+}
+
+
+print(paste("Zmienne (i ich numery kolumn) dla których w teście chisquare p < 0.05"))
+print(istotnenazwa)
+print(istotnenumer)
+
+
+p <- NULL
+i <- NULL
+n <- NULL
+tab <- NULL
---
+++ Ankieta_substancje_psychoaktywne-naprawa/README.md
@@ -0,0 +1,7 @@
+# Ankieta substancje psychoaktywne
+#### Analiza statystyczna na potrzeby referatu na konferencje Juvenes Pro Medicina 2018
+#### Na podstawie ankiety autorstwa Pauli Dybowskiej i mojego.  
+#### Analizę statystyczną przeprowadziłem sam. W regresji logistycznej pomógł mi Dr. Bandurski. 
+
+Opis kolumn (zmiennych) bazy **Dane.Rda**, na której bazują wszystkie skrypty znajduje się: **[>>TU<<](http://htmlpreview.github.io/?https://github.com/zdanowski/Ankieta_substancje_psychoaktywne/blob/master/OPIS_ZMIENNYCH.html)**  
+Wszystkie testy statystyczne oraz regresja logistyczna (wraz z obszernym komentarzem i **~wnioskami**) znajdują się w **PODSUMOWANIE_**. [>>TU<<](https://htmlpreview.github.io/?https://github.com/zdanowski/Ankieta_substancje_psychoaktywne/blob/master/PODSUMOWANIE_.html)
